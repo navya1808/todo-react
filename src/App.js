57686bd0ebf1ae3e209f7a1888ef22c1af53e3f5
Navya <navyagoyal400@gmail.com>
2021-10-16 19:30:38 +0530

    project init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import CardUI from "./Components/CardUI";
+import { useEffect, useState } from "react";
+import Form from "./Components/Form";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import UpdateForm from "./Components/UpdateForm";
+
+const getLocalItems = () => {
+  let list = localStorage.getItem('todoList');
+  console.log(list);
+  if(list){
+    return JSON.parse(localStorage.getItem('todoList'));
+  }
+  else {
+    return [];
+  }
+};
+
 
 function App() {
+  const [todoList, setTodoList] = useState(getLocalItems());
+  
+  const todoPushValue = (todoVal) => {
+    const makeId = Math.random().toString()
+    setTodoList([...todoList, { name: todoVal, id: makeId }]);
+  };
+
+  const delfunc = (afterDelArr) => {
+    setTodoList(afterDelArr);
+  };
+
+  const onDelete = (allDelete) => {
+    setTodoList(allDelete);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify( todoList ) );
+  } , [todoList]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Form
+            onTodoItem={todoPushValue}
+            arr={todoList}
+            onDeleteAllHandler={onDelete}
+          ></Form>
+          <CardUI values={todoList} onNewDelete={delfunc}></CardUI>
+        </Route>
+        <Route path="/update/:todoId" exact>
+          <UpdateForm oldVals={todoList} />
+        </Route>
+        <Route path="/" exact>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
